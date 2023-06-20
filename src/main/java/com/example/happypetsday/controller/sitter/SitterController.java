6607fca6bce26d632a5c2fedcd519f8312cc8b48
leejh1118 <dhtkan12@gmail.com>
2023-06-20 09:01:12 +0900

    WIP : 임시 커밋 이정현 0620

--- src/main/java/com/example/happypetsday/controller/sitter/SitterController.java
+++ src/main/java/com/example/happypetsday/controller/sitter/SitterController.java
@@ -1,10 +1,15 @@
 package com.example.happypetsday.controller.sitter;
 
+import com.example.happypetsday.dto.SitterApplyDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/sitter/*")
 public class SitterController {
 
